1799b96eee515c1021436b6e929a7bd019df67f7
HARRY5D <harnishdpatel@gmail.com>
2024-08-30 21:36:36 +0530

    DSA PRACT 3.2 [B]

--- DSA_CODE/dsa_3_2b.java
+++ DSA_CODE/dsa_3_2b.java
@@ -1,39 +1,28 @@
+
 import java.util.Scanner;
 
-class ListNode 
-{
+class ListNode {
     int val;
     ListNode next;
-    ListNode(int x, ListNode list1) 
 
-    { val = x; }
-
-    ListNode(int val, ListNode list1) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    ListNode(int x) {
+        val = x;
     }
-
 }
 
-class set 
-{
-    public  ListNode endOfFirstHalf(ListNode head) 
-    {
-        ListNode slow = head, fast = head;
-
-        while (fast.next != null && fast.next.next != null) 
-        {
-            slow = slow.next;
-            fast = fast.next.next;
+class set {
+    public ListNode endOfFirstHalf(ListNode head) {
+        ListNode start = head, end = head;
+        while (end.next != null && end.next.next != null) {
+            start = start.next;
+            end = end.next.next;
         }
-        return slow;
+        return start;
     }
 
-    public  ListNode reverseList(ListNode head) 
-    {
+    public ListNode reverseList(ListNode head) {
         ListNode prev = null, curr = head;
-        
-        while (curr != null) 
-        {
+        while (curr != null) {
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
@@ -45,17 +34,17 @@ class set
     public boolean isPalindrome(ListNode head) 
     {
         if (head == null) return true;
-
+        
         ListNode firstHalfEnd = endOfFirstHalf(head);
         ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
         ListNode firstPosition = head;
         ListNode secondPosition = secondHalfStart;
 
         while (secondPosition != null) 
         {
-            if (firstPosition.val != secondPosition.val) 
-            {return false;}
-
+            if (firstPosition.val != secondPosition.val) return false;
+           
             firstPosition = firstPosition.next;
             secondPosition = secondPosition.next;
         }
@@ -65,43 +54,41 @@ class set
     }
 }
 
-public class dsa_3_2b
- {
-     public static void main(String[] args) 
+public class dsa_3_2b 
+{
+    public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("ENTER TOTAL No. OF NODES IN LINKED LIST :");
         int n = sc.nextInt();
         ListNode head = null;
         ListNode current = null;
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) 
         {
             System.out.print("ENTER VALUE OF NODE " + (i + 1) + " : ");
             int val = sc.nextInt();
             if (head == null) 
             {
-                head = new ListNode(val, list1);
+                head = new ListNode(val);
                 current = head;
             } 
             else 
             {
-                current.next = new ListNode(val, list1);
+                current.next = new ListNode(val);
                 current = current.next;
             }
         }
         sc.close();
-
+        
         set s = new set();
 
         boolean isPalindrome = s.isPalindrome(head);
-
-        if(isPalindrome == true)
-        {
-        System.out.println("ITS A PALINDROME.");
-        }
-        else 
-        {
-            System.out.println("ITS NOT A PALINDROME.");
-        }
+    
+        //if(isPalindrome == true) {
+          //  System.out.println("ITS A PALINDROME.");
+        //} else {
+          //  System.out.println("ITS NOT A PALINDROME.");
+        //}
+         System.out.println(isPalindrome);
     }
 }
