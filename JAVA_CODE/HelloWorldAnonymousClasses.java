f6a6f61d4a637ad497422fdb490151bd54c3f97c
HARRY5D <harnishdpatel@gmail.com>
2024-09-29 17:33:56 +0530

    ....

---
+++ JAVA_CODE/HelloWorldAnonymousClasses.java
@@ -0,0 +1,37 @@
+public class HelloWorldAnonymousClasses {
+
+    interface HelloWorld {
+        public void greet();
+        public void greetSomeone(String someone);
+    }
+
+    public static void sayHello() {
+        class EnglishGreeting implements HelloWorld {
+            String name = "world";
+            public void greet() {
+                greetSomeone("world");
+            }
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Hello " + name);
+            }
+        }
+        HelloWorld englishGreeting = new EnglishGreeting();
+        HelloWorld frenchGreeting = new HelloWorld() {
+            String name = "Bonjour";
+            public void greet() {
+                greetSomeone("Bonjour");
+            }
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Salut " + name);
+            }
+        };
+        englishGreeting.greet();
+        frenchGreeting.greetSomeone("Mrugendra");
+    }
+
+    public static void main(String[] args) {
+        HelloWorldAnonymousClasses.sayHello();
+    }
+}
