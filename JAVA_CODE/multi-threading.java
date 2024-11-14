34ab07eeaa91dddb9f96f217e1d528a64835f152
HARRY5D <harnishdpatel@gmail.com>
2024-11-14 09:55:53 +0530

    "Updated multiple Java and Python files, added and removed code, and deleted several files including HTML templates."

--- JAVA_CODE/multi-threading.java
+++ JAVA_CODE/multi-threading.java
@@ -2,87 +2,115 @@
 
  */
 
- import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-class Storage {
+class Storage 
+{
     private int value;
     private final Lock lock = new ReentrantLock();
 
-    public void setValue(int value) {
+    public void setValue(int value) 
+    {
         lock.lock();
-        try {
+        try 
+        {
             this.value = value;
-        } finally {
+        } 
+        finally 
+        {
             lock.unlock();
         }
     }
 
-    public int getValue() {
+    public int getValue() 
+    {
         lock.lock();
-        try {
+        try 
+        {
             return value;
-        } finally {
+        } 
+        finally 
+        {
             lock.unlock();
         }
     }
 }
 
-class Counter implements Runnable {
+class Counter implements Runnable 
+{
     private final Storage storage;
     private volatile boolean running = true;
 
-    public Counter(Storage storage) {
+    public Counter(Storage storage) 
+    {
         this.storage = storage;
     }
 
     @Override
-    public void run() {
+    public void run() 
+    {
         int count = 0;
-        while (running) {
+        while (running) 
+        {
             storage.setValue(count);
             count++;
-            try {
+         
+            try 
+            {
                 Thread.sleep(1000); // Sleep for 1 second
-            } catch (InterruptedException e) {
+            } 
+            catch (InterruptedException e) 
+            {
                 Thread.currentThread().interrupt();
             }
         }
     }
 
-    public void stop() {
+    public void stop() 
+    {
         running = false;
     }
 }
 
-class Printer implements Runnable {
+class Printer implements Runnable 
+{
     private final Storage storage;
     private volatile boolean running = true;
 
-    public Printer(Storage storage) {
+    public Printer(Storage storage) 
+    {
         this.storage = storage;
     }
 
     @Override
-    public void run() {
-        while (running) {
+    public void run() 
+    {
+        while (running) 
+        {
             int value = storage.getValue();
             System.out.println("Current value: " + value);
-            try {
+            try 
+            {
                 Thread.sleep(1000); // Sleep for 1 second
-            } catch (InterruptedException e) {
+            } 
+            catch (InterruptedException e) 
+            {
                 Thread.currentThread().interrupt();
             }
         }
     }
 
-    public void stop() {
+    public void stop() 
+    {
         running = false;
     }
 }
 
- class multi_threading {
-    public static void main(String[] args) {
+ class multi_threading 
+ {
+    public static void main(String[] args) 
+    {
         Storage storage = new Storage();
         Counter counter = new Counter(storage);
         Printer printer = new Printer(storage);
@@ -93,19 +121,26 @@ class Printer implements Runnable {
         counterThread.start();
         printerThread.start();
 
-        try {
+        try 
+        {
             Thread.sleep(10000); // Let the program run for 10 seconds
-        } catch (InterruptedException e) {
+        } 
+        catch (InterruptedException e) 
+        {
+
             e.printStackTrace();
         }
 
         counter.stop();
         printer.stop();
 
-        try {
+        try 
+        {
             counterThread.join();
             printerThread.join();
-        } catch (InterruptedException e) {
+        } 
+        catch (InterruptedException e) 
+        {
             e.printStackTrace();
         }
 
