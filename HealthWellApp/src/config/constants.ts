1c7e668818718efb3d4269f397c4db2a22d57027
HARRY5D <harnishdpatel@gmail.com>
2025-02-04 21:11:05 +0530

    Add Tailwind CSS utilities and update security configuration

---
+++ HealthWellApp/src/config/constants.ts
@@ -0,0 +1,6 @@
+import { encrypt, decrypt } from 'crypto';
+import { HIPAA_COMPLIANCE_LEVEL } from './hipaa.constants';
+
+export class SecurityConfig {
+  // Security implementation code from earlier
+}
---
+++ HealthWellApp/src/config/security.ts
@@ -0,0 +1,5 @@
+export const HIPAA_COMPLIANCE_LEVEL = {
+    LEVEL_1: 'basic',
+    LEVEL_2: 'enhanced',
+    LEVEL_3: 'maximum'
+  };
