3ed01edecba3f803dfcc4ae05b536c14b6395879
HARNISH PATEL <163107332+HARRY5D@users.noreply.github.com>
2025-02-07 21:56:51 +0530

    Delete project/odoo/HealthWellApp/src/config directory

--- project/odoo/HealthWellApp/src/config/constants.ts
+++
@@ -1,6 +0,0 @@
-import { encrypt, decrypt } from 'crypto';
-import { HIPAA_COMPLIANCE_LEVEL } from './hipaa.constants';
-
-export class SecurityConfig {
-  // Security implementation code from earlier
-}
