42792a94d9b49875a27d0c62a3aab1bc50d89dfb
HARRY5D <harnishdpatel@gmail.com>
2025-04-04 07:04:24 +0530

    Remove unused files and templates from twofa module

---
+++ PYTHON_MINI_PROJECT/config.py
@@ -0,0 +1,5 @@
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "sgp.noreplydce@gmail.com"  # Replace with your email
+EMAIL_HOST_PASSWORD = "haub ylen jpof ypse"  # Replace with your app password
