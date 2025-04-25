46bdb9d8837518a07172f2c44dd8344529c2713e
HARRY5D <harnishdpatel@gmail.com>
2025-04-25 09:26:56 +0530

    Remove obsolete files and templates related to the 2FA system, including database, configuration, server logic, and HTML templates for login, registration, and verification.

--- PYTHON_MINI_PROJECT/config.py
+++
@@ -1,5 +0,0 @@
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "sgp.noreplydce@gmail.com"  # Replace with your email
-EMAIL_HOST_PASSWORD = "haub ylen jpof ypse"  # Replace with your app password
